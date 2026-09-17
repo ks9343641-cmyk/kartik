8e1d72962b55c3e459fedbf5cdda2f5966abd2b9
ks9343641-cmyk <ks9343641@gmail.com>
2026-09-17 19:24:09 +0530

    changes commited by kartik again

--- file/love.js
+++ file/love.js
@@ -164,7 +164,7 @@
             ctx.scale(0.75, 0.75);
             ctx.font = "12px,Verdana"; // 字号肿么没有用? (ˉ(∞)ˉ)
             ctx.fillText("Click Me:) ", 30, -5);
-            ctx.fillText("Penguin", 28, 10);
+            ctx.fillText("Makhana😉", 28, 10);
             ctx.restore();
         },
         clear: function() {
@@ -692,7 +692,7 @@
             ctx.scale(0.75, 0.75);
             ctx.font = "12px,Verdana"; // 字号肿么没有用? (ˉ(∞)ˉ)
             ctx.fillText("Click Me:) ", 30, -5);
-            ctx.fillText("Penguin", 28, 10);
+            ctx.fillText("Makhana😉", 28, 10);
             ctx.restore();
         },
         clear: function() {
